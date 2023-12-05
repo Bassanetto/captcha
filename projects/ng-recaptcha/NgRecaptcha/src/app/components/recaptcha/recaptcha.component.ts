48e2af0c827e002e7ce86db97babd92d40b49733
“Bassanetto” <gustavo.almeidabassanetto@gmail.com>
2023-12-05 16:04:52 -0300

    modelo recaptcha

--- projects/ng-recaptcha/NgRecaptcha/src/app/components/recaptcha/recaptcha.component.ts
+++ projects/ng-recaptcha/NgRecaptcha/src/app/components/recaptcha/recaptcha.component.ts
@@ -1,73 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { VerificarRecaptchaRequest } from 'src/app/models/verificar-recaptcha-request';
 import { RecaptchaService } from 'src/app/services/recaptcha-service.service';
 
 declare var grecaptcha: any;
 
 @Component({
   selector: 'app-ng-recaptcha',
-  template: `
-  <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-  <div>
-    <div id="meuRecaptcha" (resolved)="onCaptchaResolved($event)"></div>
-    <button type="button" (click)="enviarFormulario()">Enviar</button>
-  </div>
-`,
+  template: `<div id="recaptcha"></div>`,
 })
 export class RecaptchaComponent implements OnInit {
   private tokenDoRecaptcha: string;
+  request: VerificarRecaptchaRequest;
 
-  constructor(private recaptchaService: RecaptchaService) { }
+  constructor(private recaptchaService: RecaptchaService, private zone: NgZone) { }
 
   ngOnInit(): void {
-    let reviewRecaptchaWidget: any; // Variável para armazenar a referência ao widget do reCAPTCHA
+    this.loadRecaptchaScript();
+  }
 
-    var onloadCallback = function () {
-      setTimeout(function() {
-        grecaptcha.render('meuRecaptcha', {
-           'sitekey' : '6LdKUCMpAAAAAARKZVcUcdiRdhBOgGe4CY7ijwL-'
+  loadRecaptchaScript(): void {
+    const script = document.createElement('script');
+    script.src = `https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit`;
+    script.async = true;
+    script.defer = true;
+
+    script.onload = () => {
+      window['onloadCallback'] = () => {
+        this.zone.run(() => {
+          grecaptcha.render('recaptcha', {
+            'sitekey': '6LdKUCMpAAAAAARKZVcUcdiRdhBOgGe4CY7ijwL-',
+            'callback': (response: string) => this.onCaptchaResolved(response),
+          });
         });
-      }, 1000);
+      };
     };
 
-    console.log(onloadCallback); // Isso apenas loga a função, não a executa
-
-    // Chame a função onloadCallback para iniciar o reCAPTCHA
-    onloadCallback();
-
-    const recaptchaScript = document.createElement('script');
-    recaptchaScript.src = `https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit`;
-    recaptchaScript.async = true;
-    recaptchaScript.defer = true;
-
-    document.head.appendChild(recaptchaScript);
+    document.head.appendChild(script);
   }
 
   onCaptchaResolved(captchaResponse: string): void {
-    // Faça algo com a resposta do reCAPTCHA, como enviar para o servidor
-    console.log('Resposta do reCAPTCHA:', captchaResponse);
-
     this.tokenDoRecaptcha = captchaResponse;
-    // Agora você pode enviar a resposta para o backend
-    this.recaptchaService.verificarRecaptcha(captchaResponse)
-      .subscribe((resposta) => {
-        console.log(resposta)
+
+    this.request = {
+      token: captchaResponse
+    }
+
+    this.recaptchaService.verificarRecaptcha(this.request).subscribe(
+      async res => {
+        console.log(res)
       });
-  }
-
-  enviarFormulario(): void {
-    //  Agora você pode usar this.tokenDoRecaptcha para enviar o token junto com os dados do formulário
-    const dadosDoFormulario = {/* Seus dados do formulário aqui */ };
-
-    this.recaptchaService.enviarFormulario(dadosDoFormulario, this.tokenDoRecaptcha)
-      .subscribe(
-        (resposta) => {
-          console.log('Formulário enviado com sucesso:', resposta);
-          //      Lógica adicional, se necessário
-        },
-        (erro) => {
-          console.error('Erro ao enviar o formulário:', erro);
-          //      Lógica para lidar com o erro, se necessário
-        }
-      );
   }
 }
--- projects/ng-recaptcha/NgRecaptcha/src/app/services/recaptcha-service.service.ts
+++ projects/ng-recaptcha/NgRecaptcha/src/app/services/recaptcha-service.service.ts
@@ -2,34 +2,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { VerificarRecaptchaRequest } from '../models/verificar-recaptcha-request';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RecaptchaService {
-  private apiUrl = 'URL_DO_SEU_BACKEND'; // Substitua pela URL do seu backend
-
-  constructor(private http: HttpClient) {}
-
-  verificarRecaptcha(captchaResponse: string): Observable<any> {
-    // Faça algo com a resposta do reCAPTCHA, como enviar para o servidor
-    console.log('Resposta do reCAPTCHA:', captchaResponse);
+  private apiUrl = 'http://localhost:5245/api/captcha';
   
-    // Agora você pode enviar a resposta para o backend
-    return this.http.post('/api/verificar-recaptcha', { token: captchaResponse });
-  }
 
-  enviarFormulario(dadosDoFormulario: any, tokenDoRecaptcha: string): Observable<any> {
-    // Adicione a lógica para enviar os dados para o backend
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-    });
+  constructor(private http: HttpClient) { }
 
-    const body = {
-      dadosDoFormulario,
-      tokenDoRecaptcha,
-    };
+  verificarRecaptcha(request: VerificarRecaptchaRequest) {
+    console.log('Resposta do reCAPTCHA:', request);
 
-    return this.http.post(this.apiUrl, body, { headers });
+    return this.http.post(`${this.apiUrl}/verificar-recaptcha`, request).pipe(o => o);
   }
 }
